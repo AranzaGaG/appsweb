51a2580df40201ec06b9cd3d773d200f7c3f2b2d
AranzaG <aranza.gavia@gmail.com>
2024-03-05 19:22:56 -0600

    tarea de nombre

---
+++ script.js
@@ -0,0 +1,12 @@
+// Función que conteste el nombre con alert.
+/*
+ Pedir el nombre de la persona usando prompt()
+ Mostrar el nombre de la persona con alert()
+ */
+
+
+function preguntarNombreAPersona() {
+    let message = prompt("¿Cual es tu nombre?");
+    alert(`Hola ${message}`);
+}
+
